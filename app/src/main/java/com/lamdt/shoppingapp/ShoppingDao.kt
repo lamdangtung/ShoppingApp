e940e79b5d4b0b672a322077a825a197d3dd3688
lamdt <lamdt.nws@gmail.com>
2023-08-01 17:15:09 +0700

    feat: add shopping database

---
+++ app/src/main/java/com/lamdt/shoppingapp/ShoppingDao.kt
@@ -0,0 +1,20 @@
+package com.lamdt.shoppingapp
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ShoppingDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+
+    @Query(value = "SELECT * FROM shopping_item")
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>>
+}
